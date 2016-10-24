eb655382a2817f44d10ac165179485274708e5c1
alumno <alumno@aula01-13>
2016-10-24 19:46:28 +0200

    Ejercicio Terminado primos

--- Numprimos/src/numprimos/Numprimos.java
+++ Numprimos/src/numprimos/Numprimos.java
@@ -4,18 +4,61 @@
  * and open the template in the editor.
  */
 package numprimos;
-
+import java.util.Scanner;
 /**
  *
  * @author alumno
  */
 public class Numprimos {
+    
+    //Devuelve truesi es primo, false en caso contrario
+     public static boolean comprobarprimo(int num1){
+       
+        boolean primo=false;
 
-    /**
-     * @param args the command line arguments
-     */
+        for (int i=2; i<num1;i++){
+             if (num1%i==0){
+                 primo=true;
+                 i=num1+1;
+             }
+
+         }
+        return primo; 
+    }
+     
+     public static int siguienteprimo(int num1){
+        int primo=0; 
+        boolean bandera=true;
+        for (int i=(num1+1);bandera==true; i++){
+            if (comprobarprimo(i)){
+                bandera=false;
+                primo=i;
+            }
+            
+        }
+        
+        return primo;
+     }
+             
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner leer=new Scanner (System.in);
+        boolean bandera =true;
+        int numero=0;
+
+        
+        while (bandera){
+            System.out.println("Escribe un número");
+            numero = leer.nextInt();
+            if (comprobarprimo(numero)){
+                System.out.println("El número "+ numero+ " es primo");
+                System.out.println("El siguiente primo es "+ siguienteprimo(numero));
+            } else{
+                System.out.println("El número "+ numero+ " no es primo");
+                System.out.println("El siguiente primo es "+ siguienteprimo(numero));
+                
+            }
+        }
+      
     }
     
 }
